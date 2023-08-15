f1c80350dea6cbb2abf640ddb98499eef45f77db
juedu36 <120651477+devJuliaLeal@users.noreply.github.com>
2023-08-15 10:54:21 -0300

    novas funcoes

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const startPieces = [
 
 rook, knight, bishop, queen, king, bishop, knight, rook, 
 pawn,pawn, pawn, pawn, pawn, pawn,pawn, pawn, 
-, '', '', '', '', '', '', '', '',
-, '', '', '', '', '', '', '', '',
-, '', '', '', '', '', '', '', '',
-, '', '', '', '', '', '', '', '',
+ '', '', '', '', '', '', '', '',
+ '', '', '', '', '', '', '', '',
+ '', '', '', '', '', '', '', '',
+ '', '', '', '', '', '', '', '',
 pawn,pawn, pawn, pawn, pawn, pawn,pawn, pawn, 
 rook, knight, bishop, queen, king, bishop, knight, rook, 
 
@@ -20,13 +20,41 @@ rook, knight, bishop, queen, king, bishop, knight, rook,
 
 
 function createBoard(){
-    startPieces.forEach((startPiece) =>
+    startPieces.forEach((startPiece, i ) =>
         {
             const square = document.createElement('div')
             square.classList.add('square')
-            square.classList.add('beige')
+            square.innerHTML = startPiece 
+           square.firstChild?.setAttribute('draggable', 'true')
+
+
+            square.setAttribute('square-id', i)
+           
+           const row = Math.floor((63-i)/8)+1 
+                if(row %2 == 0){
+                    square.classList.add(i % 2 === 0? "beige":"brown")
+                }
+                else{ square.classList.add(i % 2 === 0 ? "brown":"beige")}
+               
+                if (i <= 15) {
+                    const svgElement = square.querySelector('svg'); 
+                    if (svgElement) {
+                        svgElement.classList.add('black');
+                    }
+                }
+                if (i >= 48) {
+                    const svgElement = square.querySelector('svg'); 
+                    if (svgElement) {
+                        svgElement.classList.add('white');
+                    }
+                }
+
             gameBoard.append(square)
         }
+           
+
+
+
     )
 }
 
