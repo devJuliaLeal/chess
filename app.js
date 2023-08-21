5548d58c39c8857767e964d82dc7373885bebd75
juedu36 <120651477+devJuliaLeal@users.noreply.github.com>
2023-08-21 18:23:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -128,21 +128,23 @@ function dragDrop(e) {
             
             const starterRow = [8, 9, 10, 11, 12, 13, 14, 15];
             
-            if (starterRow.includes(startId) && startId + width * 2 === targetId || 
-            startId + width === targetId || 
-            startId + width - 1 === targetId && document.querySelector(`[square-id="${startId + width - 1}]`).firstChild ||
-            startId + width + 1 === targetId && document.querySelector(`[square-id="${startId + width + 1}]`).firstChild
-            
-            ) {
-                return true;
+            if (starterRow.includes(startId)) {
+                if (startId + width * 2 === targetId || startId + width === targetId) {
+                    return true;
+                }
+            } else {
+                if (startId + width - 1 === targetId && document.querySelector(`[square-id="${startId + width - 1}"]`).firstChild ||
+                    startId + width + 1 === targetId && document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild
+                ) {
+                    return true;
+                }
             }
+            
            
             break;
       
     }
 }
-
-
 
 
 
