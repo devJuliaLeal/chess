cae19f78ce5a2cbd9807ecaa71c29b7f0496f3ac
juedu36 <120651477+devJuliaLeal@users.noreply.github.com>
2023-08-23 15:18:39 -0300

    update app.js

--- app.js
+++ app.js
@@ -85,47 +85,47 @@ function dragOver(e){
 }
 
 function dragDrop(e) {
+    e.stopPropagation();
 
-    e.stopPropagation()
- 
+    const correctGo = draggedElement.firstChild.classList.contains(playerGo);
+    const taken = e.target.classList.contains('piece');
+    const valid = checkIfValid(e.target);
+    const opponentGo = playerGo === 'white' ? 'black' : 'white';
+    const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo);
 
-    const correctGo = draggedElement.firstChild.classList.contains(playerGo)
-    const taken = e.target.classList.contains('piece')
-    const valid = checkIfValid(e.target)
-    const opponentGo = playerGo === 'white' ? 'black' : 'white'
-    const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
-
-    if (correctGo){
-        if(takenByOpponent && valid){
-            e.target.parentNode.append(draggedElement)
-            e.target.remove()
-            changePlayer()
-            return
-       }
-        if(taken){
-            infoDisplay.textContent = "NO"
-            setTimeout(() => infoDisplay.textContent = "", 2000)
-            return
+    if (correctGo) {
+        if (takenByOpponent && valid) {
+            e.target.parentNode.append(draggedElement);
+            e.target.remove();
+            changePlayer();
+            return;
         }
-        if(valid){
-            e.target.append(draggedElement)
-            changePlayer()
-            return
+        if (taken) {
+            infoDisplay.textContent = "Movimento inválido";
+            setTimeout(() => infoDisplay.textContent = "", 2000);
+            return;
         }
-
-
+        if (valid) {
+            e.target.append(draggedElement);
+            changePlayer();
+            return;
+        }
     }
+}
 
 
-  }
+
+  
   function checkIfValid(target) {
     const targetId = Number(target.getAttribute('square-id')) || Number(target.parentNode.getAttribute('square-id'));
     const startId = Number(startPositionId);
     const piece = draggedElement.id;
-    console.log('targetId', targetId);
-    console.log('startId', startId);
-    console.log(piece);
+    const rowDiff = Math.floor(targetId / width) - Math.floor(startId / width);
+    const colDiff = targetId % width - startId % width;
+
+
     switch (piece) {
+//=================================================PEAO============================================================ 
         case 'pawn':
             
             const starterRow = [8, 9, 10, 11, 12, 13, 14, 15];
@@ -138,7 +138,7 @@ function dragDrop(e) {
                 return true;
             }
             break;
-            
+//===============================================CAVALO========================================================================                   
         case 'knight': 
         if(
             startId+ width *2 + 1 === targetId ||
@@ -157,22 +157,90 @@ function dragDrop(e) {
         }
       break;
 
-        case 'bishop':
-        if (
-            startId + width +1 === targetId ||
-            startId +width*2 + 2 && !document.querySelector(`[square-id="${startId + width +1}"]`).firstChild ||
-            startId +width*3 + 3 && !document.querySelector(`[square-id="${startId + width +1}"]`).firstChild ||
-            
+ //===============================================BISPO========================================================================             
+      case 'bishop':
 
-        ){
-            return true;
+        if (Math.abs(rowDiff) === Math.abs(colDiff)) {
+            const rowDirection = rowDiff > 0 ? 1 : -1;
+            const colDirection = colDiff > 0 ? 1 : -1;
+            let currentRow = Math.floor(startId / width) + rowDirection;
+            let currentCol = startId % width + colDirection;
+
+            while (currentRow !== Math.floor(targetId / width)) {
+                const cell = document.querySelector(`[square-id="${currentRow * width + currentCol}"]`);
+                if (cell.firstChild) {
+                    return false 
+                }
+                currentRow += rowDirection;
+                currentCol += colDirection;
+            }
+
+            return true
+        }
+        break;
+//=============================================TORRE=========================================================================
+        case 'rook':
+
+    if ((rowDiff === 0 && colDiff !== 0) || (colDiff === 0 && rowDiff !== 0)) {
+        const step = rowDiff !== 0 ? (rowDiff > 0 ? width : -width) : (colDiff > 0 ? 1 : -1);
+        let currentId = startId + step;
+
+        while (currentId !== targetId) {
+            const cell = document.querySelector(`[square-id="${currentId}"]`);
+            if (cell.firstChild) {
+                return false; 
+            }
+            currentId += step;
         }
 
-
+        return true; 
     }
+    break;
+//=============================================RAINHA=========================================================================
+case 'queen':
+    if (
+        // Movimento da torre (horizontal e vertical)
+        ((rowDiff === 0 && colDiff !== 0) || (colDiff === 0 && rowDiff !== 0)) ||
+        // Movimento do bispo (diagonais)
+        (Math.abs(rowDiff) === Math.abs(colDiff))
+    ) {
+        const rowDirection = rowDiff === 0 ? 0 : (rowDiff > 0 ? 1 : -1);
+        const colDirection = colDiff === 0 ? 0 : (colDiff > 0 ? 1 : -1);
+        
+        let currentRow = Math.floor(startId / width) + rowDirection;
+        let currentCol = startId % width + colDirection;
+
+        while (currentRow !== Math.floor(targetId / width) || currentCol !== targetId % width) {
+            const cell = document.querySelector(`[square-id="${currentRow * width + currentCol}"]`);
+            if (cell.firstChild) {
+                return false; // There's a piece in the way
+            }
+            
+            currentRow += rowDirection;
+            currentCol += colDirection;
+        }
+
+        return true; // All clear for the queen to move
+    }
+    break;
+//=============================================REI E ROQUE=========================================================================
+case 'king':
+    const validOffsets = [-9, -8, -7, -1, 1, 7, 8, 9];
+    const offset = targetId - startId;
+
+    if (validOffsets.includes(offset)) {
+        const targetSquare = document.querySelector(`[square-id="${targetId}"]`);
+
+        // Verifique se a casa de destino está vazia ou tem uma peça do oponente
+        if (!targetSquare.firstChild || targetSquare.firstChild.classList.contains(opponentGo)) {
+            return true;
+        }
+        break;
+    }
+    
+
 }
-
-
+  
 
 
  function changePlayer(){
@@ -187,7 +255,7 @@ function dragDrop(e) {
         playerDisplay.textContent = 'black'
     }
  } 
- 
+
  function reverseIds(){
 const allSquares =  document.querySelectorAll(".square")
 
